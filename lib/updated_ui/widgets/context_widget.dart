0c5ee9955ea5cb26e09f56571dc1a8ae33084216
DenisTokar <45533926+DenisBlur@users.noreply.github.com>
2023-01-24 18:18:46 +0500

    New context menu, and update button ui

--- lib/updated_ui/widgets/context_widget.dart
+++ lib/updated_ui/widgets/context_widget.dart
@@ -1,7 +1,11 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:gibbon_music/constants/style_consts.dart';
 import 'package:gibbon_music/domain/models/playlist.dart';
+import 'package:gibbon_music/extensions/string.dart';
+import 'package:gibbon_music/router.dart';
 import 'package:gibbon_music/updated_ui/controls/buttons.dart';
+import 'package:gibbon_music/updated_ui/widgets/ImageThumbnail.dart';
 import 'package:provider/provider.dart';
 import 'package:smooth_corner/smooth_corner.dart';
 
@@ -23,11 +27,12 @@ class _ContextWidgetState extends State<ContextWidget> {
   @override
   Widget build(BuildContext context) {
     UxProvider ux = context.watch();
-    print("$x : $y");
     return StreamBuilder<TrackContextDetail>(
       stream: ux.onChangeDetails.stream,
       builder: (context, snapshot) {
         if (snapshot.hasData && ux.isContextMenu) {
+
+          var track = snapshot.data!.track;
           var x = snapshot.data!.details.globalPosition.dx.toInt();
           var y = snapshot.data!.details.globalPosition.dy.toInt();
 
@@ -49,37 +54,63 @@ class _ContextWidgetState extends State<ContextWidget> {
                     x -= 266;
                   }
                   if (y > AppConsts.pageSize(context).height / 2) {
-                    y -= (25*2)+16;
+                    int btnCount = track.artists!.length + 2;
+                    y -= (30*btnCount)+100;
                   }
                   return Positioned(
                     left: x.toDouble(),
                     top: y.toDouble(),
+                    width: 250,
                     child: FadeInUp(
                       duration: Duration(milliseconds: 100),
                         child: SmoothContainer(
-                      padding: const EdgeInsets.all(8),
+                      padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
                       key: _keyRed,
                       smoothness: 1,
                       borderRadius: BorderRadius.circular(16),
-                      color: Colors.teal,
+                      color: FluentTheme.of(context).cardColor,
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          SizedBox(
-                            width: 250,
-                            height: 25,
-                            child: GButton(onPressed: () {
-                              context.read<NewPlaylist>().addTrackToEnd(snapshot.data!.track);
-                              ux.isContextMenu = false;
-                            }, title: "Добавить в конец"),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(16),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black.withOpacity(.1),
+                                      offset: const Offset(0,4),
+                                      blurRadius: 5
+                                    )
+                                  ]
+                                ),
+                                child: ImageThumbnail(url: track.ogImage.toString().linkImage(100), height: 56, width: 56, radius: 16,),
+                              ),
+                              AppConsts.defaultHSpacer,
+                              Expanded(child: Text(track.title.toString(), style: AppStyle.subTitle(context), maxLines: 1),),
+                            ],
                           ),
-                          SizedBox(
-                            width: 250,
-                            height: 25,
-                            child: GButton(onPressed: () {
-                              context.read<NewPlaylist>().addTrackAfterCurrent(snapshot.data!.track);
-                              ux.isContextMenu = false;
-                            }, title: "Добавить после текущего"),
-                          )
+                          AppConsts.defaultVSpacer,
+                          GButton(onPressed: () {
+                            context.read<NewPlaylist>().addTrackToEnd(track);
+                            ux.isContextMenu = false;
+                          }, title: "Добавить в конец"),
+                          AppConsts.smallVSpacer,
+                          GButton(onPressed: () {
+                            context.read<NewPlaylist>().addTrackAfterCurrent(track);
+                            ux.isContextMenu = false;
+                          }, title: "Добавить после текущего"),
+                          AppConsts.defaultHSpacer,
+                          Text("Исполнители", style: AppStyle.subTitle(context),),
+                          AppConsts.smallVSpacer,
+                          for(var e in track.artists!) Padding(padding: EdgeInsets.only(bottom: 8), child: GButton(onPressed: () {
+                            AppRouter().gotoArtist(context, e.id);
+                            ux.isContextMenu = false;
+                          }, title: e.name!),),
                         ],
                       ),
                     )),
